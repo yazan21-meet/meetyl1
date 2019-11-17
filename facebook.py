f8156673c48d2d089586fb1083c45fa9494eb75e
yazan21-meet <52988378+yazan21-meet@users.noreply.github.com>
2019-11-17 11:50:40 -0500

    Add files via upload

--- facebook.py
+++ facebook.py
@@ -1,3 +1,7 @@
+class post():
+	def __init__(self,user,content):
+		self.user=user
+		self.content=content
 class user():
 	def __init__(self,name,email,password):
 		self.name=name
@@ -6,12 +10,31 @@ class user():
 		self.friends_list=[]
 		self.posts=[]
 	def add_friend(self,email):
-		friends.append(user1)
-		print(self.name+ "has added"+ self.email)
+		self.friends_list.append(user1)
+		print(self.name+ "has added"+ email)
 	def remove_friend(self,email):
-		friends.remove(user1)
-		print(self.name+ "has removed"+ self.email)
+		self.friends_list.remove(user1)
+		print(self.name + "has removed"+ email)
 	def post(self,text):
 		self.posts.append(text)
 		print(self.name+ "has posted"+ text)
-user1=user("yazan","yazandakkak@facebook","123")
+	def get_userInfo(self):
+		print("name: "+self.name)
+		print("email: "+self.email)
+		print("password: "+self.password)
+		print("friends: "+str(self.friends_list))
+		print("posts: "+str(self.posts))
+
+user1=user("yazan "," yazandakkak@facebook"," 123")
+user2=user("osama "," osamahijazi@facebook"," 234")
+user2.add_friend(" yazandakkak@facebook")
+user2.add_friend("samirhazboun@facebook")
+user2.post(" a photo")
+user2.remove_friend(" samirhazboun@facebook")
+user2.get_userInfo()
+user3=user("samir "," samirhazboun@facebook","543")
+user3.add_friend(" yazandakkak@facebook")
+user3.add_friend(" osamahijazi2facebook")
+user3.post(" an article")
+user3.remove_friend(" osamahijazi@facebook")
+user3.get_userInfo()
